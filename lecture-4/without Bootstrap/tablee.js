3f1ed63c4c43dc75bd72f0cdd205ae2107a5031c
DrashtiApani <darshtiapani4@gmail.com>
2024-12-23 16:02:59 +0530

    22/12/2

---
+++ lecture-4/without Bootstrap/tablee.js
@@ -0,0 +1,70 @@
+
+var avg = [85,89,88,76,79]; //using array
+        var tbl = 
+        `
+            <table align="center" style= "border: 1px solid black">
+                
+                <tr style="background-color:ghostwhite;color:midnightblue; padding:25px 25px;border: 1px solid black" >
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Run</th>
+                    <th>Centuries</th>
+                    <th>Strike Rate</th>
+                    <th>Avg</th>
+                    <th>img</th>
+                </tr>
+                
+                <tr>
+                    <td>1</td>
+                    <td>Rohit</td>
+                    <td>6452</td>
+                    <td>123</td>
+                    <td>97</td>
+                    <td>${avg[0]}</td>
+                    <td><img src = "https://tse1.mm.bing.net/th?id=OIP.Mmont1MY7D7pocEkdVWXMAHaFj&pid=Api&P=0&h=180" style="width:80px;height: 50px;"></td>
+                </tr>    
+
+                <tr>
+                    <td>2</td>
+                    <td>Virat</td>
+                    <td>6210</td>
+                    <td>108</td>
+                    <td>92</td>
+                    <td>${avg[1]}</td>
+                    <td><img src ="https://tse4.mm.bing.net/th?id=OIP.S573pflHo6NVWjHGPgoX8wHaE7&pid=Api&P=0&h=180" style="width:80px;height: 50px;"></td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>Suryakumar</td>
+                    <td>5802</td>
+                    <td>99</td>
+                    <td>155</td>
+                    <td>${avg[2]}</td>
+                    <td><img src ="https://tse1.mm.bing.net/th?id=OIP.w8cVjziUn959PW4rlnK4_wHaJR&pid=Api&P=0&h=180" style="width:80px;height: 50px;"></td>
+                </tr>
+                
+                <tr>
+                    <td>4</td>
+                    <td>Hardik</td>
+                    <td>5688</td>
+                    <td>75</td>
+                    <td>125</td>
+                    <td>${avg[3]}</td>
+                    <td><img src ="https://tse4.mm.bing.net/th?id=OIP.kjig0hNAJn1TzrofB4Z1ZgHaEK&pid=Api&P=0&h=180" style="width:80px;height: 50px;"></td>
+                </tr>
+
+                <tr>
+                    <td>5</td>
+                    <td>Shubman Gill</td>
+                    <td>5674</td>
+                    <td>77</td>
+                    <td>75</td>
+                    <td>${avg[4]}</td>
+                    <td><img src ="https://tse4.mm.bing.net/th?id=OIP.ra_ErSnKbI6F8pkhO1fQsQHaEK&pid=Api&P=0&h=180" style="width:80px;height: 50px;"></td>
+                </tr>
+                
+            </table>
+                
+        `
+        document.getElementById("Table").innerHTML = tbl;
